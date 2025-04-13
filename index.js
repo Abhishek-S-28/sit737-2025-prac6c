e5b0380d13fc191c70a68a7db2b052f011de6879
Abhishek Srinivasan <abhisheksrini2001@gmail.com>
2025-04-13 18:44:20 +1000

    Initial commit for SIT737 2025 Prac6C - Kubernetes deployment and update

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
-  res.send('Hello from 737kubernetes Node.js app!');
+ res.send('This is an UPDATED version of the 737kubernetes app!');
 });
 
 app.listen(PORT, () => {
